81575d99ba0be40396be35cbd0e7e0807d4b6fbc
Paulius Velesko <paulius.velesko@intel.com>
2019-10-11 20:54:02 +0000

    added context and device

--- main.cpp
+++ main.cpp
@@ -5,21 +5,23 @@ using namespace cl::sycl;
 
 int main(int, char**) {
   int num[3] = {1, 1, 0};
-  queue queue(cpu_selector{});
+  queue q(gpu_selector{});
+  device dev = q.get_device();
+  context ctx = q.get_context();
   std::cout << "Running on "
-            << queue.get_device().get_info<info::device::name>()
+            << dev.get_info<info::device::name>()
             << std::endl;
 
   { // buffer scope
     buffer<int, 1> num_d(num, range<1>(3));
-    queue.submit([&] (handler& cgh) 
-      { // queue scope
+    q.submit([&] (handler& cgh) 
+      { // q scope
         auto num = num_d.get_access<access::mode::read_write>(cgh);
         cgh.single_task<class oneplusone>([=] 
         {
           num[2] = num[0] + num[1];
         } ); // end task scope
-      } ); // end queue scope
+      } ); // end q scope
   } // end buffer scope
 
   std::cout << "1 + 1 = " << num[2] << std::endl;
