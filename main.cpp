621b49be4998b8db160636e18c6c76c6f1e54583
Paulius Velesko <paulius.velesko@intel.com>
2019-10-11 20:15:46 +0000

    cleanup

--- main.cpp
+++ main.cpp
@@ -5,22 +5,24 @@ using namespace cl::sycl;
 
 int main(int, char**) {
   int num[3] = {1, 1, 0};
-  {
   queue queue(cpu_selector{});
-    std::cout << "Running on "
-              << queue.get_device().get_info<info::device::name>()
-              << "\n";
-  buffer<int, 1> num_d(num, range<1>(3));
-  queue.submit([&] (handler& cgh) {
-      auto num = num_d.get_access<access::mode::read_write>(cgh);
-      cgh.single_task<class oneplusone>([=] {
-        num[2] = num[0] + num[1];
-    } );
-    } );
-  }
+  std::cout << "Running on "
+            << queue.get_device().get_info<info::device::name>()
+            << std::endl;
+
+  { // buffer scope
+    buffer<int, 1> num_d(num, range<1>(3));
+    queue.submit([&] (handler& cgh) 
+      { // queue scope
+        auto num = num_d.get_access<access::mode::read_write>(cgh);
+        cgh.single_task<class oneplusone>([=] 
+        {
+          num[2] = num[0] + num[1];
+        } ); // end task scope
+      } ); // end queue scope
+  } // end buffer scope
 
   std::cout << "1 + 1 = " << num[2] << std::endl;
 
   return 0;
 }
-
