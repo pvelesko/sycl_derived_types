fc6f255e819bc985687fe3e1fb75db98e94bd129
Paulius Velesko <paulius.velesko@intel.com>
2020-01-22 14:04:47 +0000

    trying to work with aosoa

--- main.cpp
+++ main.cpp
@@ -8,32 +8,86 @@ class Particle {
   int data;
 };
 
-//class ParticleSoA {
-//  int* a;
-//
-//  public:
-//  ParticleSoA(int n) {
-//    a = static_cast<int*>(malloc(n * sizeof(int)));
-//  };
-//};
+class ParticleSoA {
+  private:
+  int size = 0;
+  int sum = 0;
+  int* a_array;
+  buffer<int, 1> *a_buffer;
+
+  public:
+  ParticleSoA() {};
+  ParticleSoA(int n) {
+    size = n;
+    a_array = static_cast<int*>(calloc(n, sizeof(int)));
+    a_buffer = new buffer<int, 1>(a_array, n);
+  };
+
+  auto *get_array() { return a_array;};
+  auto *get_buffer() { return a_buffer;};
+  auto  get_size() { return size;};
+
+  // TODO
+  auto  get_sum() { 
+    int s = 0; 
+    for (int i = 0; i < size; i++) s += a_array[i];
+    return s;
+    ;};
+  void  set_sum(int s) { sum = s;};
+};
+
 
 int main(int argc, char** argv) {
   // Parse args
-  int n = std::atoi(argv[1]);
-  n = 3;
+  int n;
+  if (argc > 1) {
+    n = std::atoi(argv[1]);
+  } else {
+    n = 3;
+  }
   std::cout << "Using N = " << n << std::endl;
 
 
+  // AoS
   std::vector<Particle> ptl(n);
-  ptl[0].data = 1;
-  ptl[1].data = 1;
-  ptl[2].data = 0;
-  //ParticleSoA ptlSoA(n);
-  //{
-  //  buffer<PartielSoA, 1> num_d(num, range<1>(3));
-  //}
 
-  int num[3] = {1, 1, 0};
+  // SoA
+  ParticleSoA ptl_soa(n);
+
+  // AoSoA
+  std::vector<ParticleSoA> ptl_aosoa(n);
+  for (int i = 1; i < n; i++) {
+    ptl_aosoa[i] = ParticleSoA(n);
+  }
+  ptl_aosoa[0] = ptl_soa;
+
+  // lambda to be called for host
+  auto incr = [=](ParticleSoA  &soa_in, Particle *aos_in, int idx) {
+    soa_in.get_array()[idx] += 1;
+  };
+
+//  // lambda to be called using host accessors
+//  auto incr = [=](ParticleSoA  &soa_in, Particle *aos_in, int idx) {
+//    soa_in.get_array()[idx] += 1;
+//  };
+
+
+//  // lambda to be called for SYCL, I want to preserve code layout of passing structs
+//  // Trying to figure out if this is possible...
+//  auto incr_sycl = [=](ParticleSoA  &soa_in, Particle *aos_in, int idx) {
+//    //soa_in.get_accessor()[idx] = soa_in.get_accessor()[idx] + 1;
+//    (soa_in.a_accessor)[idx] = -1;
+//  };
+
+
+  // host call 
+  for (int i = 0; i < ptl_aosoa[0].get_size(); i++)
+    incr(ptl_aosoa[0], ptl.data(), i);
+
+
+  // sum == n
+  std::cout << "ptl_aosoa[0] sum = " << ptl_aosoa[0].get_sum() << std::endl;
+
   queue q(gpu_selector{});
   device dev = q.get_device();
   context ctx = q.get_context();
@@ -41,24 +95,30 @@ int main(int argc, char** argv) {
             << dev.get_info<info::device::name>()
             << std::endl;
 
-  { // buffer scope
-    //buffer<Particle, 1> test(&ptl, range<1>(1));
-    buffer<int, 1> num_d(num, range<1>(3));
-    buffer<Particle, 1> ptl_d(ptl.data(), range<1>(n));
-    q.submit([&] (handler& cgh) 
-      { // q scope
-        auto num = num_d.get_access<access::mode::read_write>(cgh);
-        auto ptl = ptl_d.get_access<access::mode::read_write>(cgh);
-        cgh.single_task<class oneplusone>([=] 
-        {
-          ptl[2].data = ptl[0].data + ptl[1].data;
-          num[2] = num[0] + num[1];
-        } ); // end task scope
-      } ); // end q scope
-  } // end buffer scope
+  q.submit([&] (handler& cgh) mutable { // q scope
+    //accessor<int, 1, access::mode:read_write> a0 = accessor<int, 1, access::mode::read_write, access::target::global_buffer>(ptl_aosoa[0].get_buffer(), cgh);
+    accessor<int, 1, access::mode::read_write> a0 = accessor<int, 1, access::mode::read_write, access::target::global_buffer>(*ptl_aosoa[0].get_buffer(), cgh);
 
-  std::cout << "1 + 1 = " << num[2] << std::endl;
-  std::cout << "ptl 1 + 1 = " << ptl[2].data << std::endl;
+    // What if I want to create an array of such accessors? Guessing that I can't
+    std::vector<accessor<int, 1, access::mode::read_write>> avec;
+    accessor<int, 1, access::mode::read_write>* aarr[n];
+    aarr[0] = &a0;
+    avec.push_back(a0);
+    cgh.parallel_for<class oneplusone>(range<1>(ptl_aosoa[0].get_size()), [=] (id<1> idx) mutable {
+      //avec[0][0] += 1; // compiler crash
+      aarr[0][0] += 1; // compiler crash
+      //a0[idx] += 1; // works
+    } ); // end task scope
+  } ); // end q scope
+
+  int buffersum = 0;
+  for (int i = 0; i < ptl_aosoa[0].get_size(); i++) {
+    buffersum += ptl_aosoa[0].get_buffer()->get_access<access::mode::read>()[i];
+  }
+  std::cout << "ptl_aosoa[0] sum = " << ptl_aosoa[0].get_sum() << std::endl;
+  std::cout << "ptl_aosoa[0] buffer sum = " << buffersum << std::endl;
+  //std::cout << "1 + 1 = " << ptl_soa.get_array()[0] << std::endl;
+  //std::cout << "ptl 1 + 1 = " << ptl[2].data << std::endl;
 
   return 0;
 }
--- makefile
+++ makefile
@@ -6,7 +6,10 @@ intel: main.cpp
 	clang++ -std=c++14 -fsycl -lsycl -lOpenCL ./main.cpp -o intel
 
 codeplay: main.cpp
-	compute++ -std=c++11 -sycl -sycl-driver -lComputeCpp ./main.cpp -o codeplay
+	compute++ -std=c++14 -sycl -sycl-driver -lComputeCpp ./main.cpp -o codeplay
+
+hipsycl: main.cpp
+	syclcc-clang-wrapper -std=c++14 ./main.cpp -o hipsycl 
 
 clean:
 	rm -f ./intel ./codeplay
